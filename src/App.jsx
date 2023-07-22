8460e84d93a44f592be305cf8832bdb441962f59
jakariachynabil10 <jakariachynabil@gmail.com>
2023-07-22 07:54:05 +0600

    college card section is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css'
 import Banner from './Components/Banner';
+import CollegeCard from './Components/CollegeCardSection/CollegeCard';
 
 
 function App() {
   return (
     <>
       <Banner></Banner>
+      <CollegeCard></CollegeCard>
     </>
   )
 }
